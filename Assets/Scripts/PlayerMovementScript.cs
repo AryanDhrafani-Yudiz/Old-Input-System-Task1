4da26a646924c6436c7c804330e53d8fd9bd54a8
AryanDhrafani-Yudiz <aryan.dhrafani@yudiz.com>
2024-02-29 10:26:46 +0530

    Update PlayerMovementScript.cs

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -51,22 +51,12 @@ public class PlayerMovementScript : MonoBehaviour
                     if (touch.phase == TouchPhase.Began)    // When First Clicked At A Point On Screen
                     {
                         Vector2 pos = touch.position;
-                        pos.x = (pos.x - width) / width;
-                        pos.y = (pos.y - height) / height;
-                        position1 = new Vector3(pos.x, 0.0f, pos.y);
+                        FindStartingLocation(pos);
                     }
-                    else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)   // When The Joystick Is Being Moved
+                    else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)   // When The Joystick Is Being Moved Or Is Stationary
                     {
                         Vector2 pos = touch.position;
-                        pos.x = (pos.x - width) / width;
-                        pos.y = (pos.y - height) / height;
-                        position2 = new Vector3(pos.x, 0.0f, pos.y);
-                        diff = (position2 - position1);                           // Difference In Vector3
-                        relativeSpeed = Mathf.Clamp(diff.magnitude, 0.3f, 0.7f);  // Clamps The Magnitude Of Difference (Float Value Of Difference) Between Required Min,Max Values
-                        currPosition = transform.position;
-                        endPosition = currPosition + diff;
-                        transform.LookAt(endPosition);                            // Cube Looks Towards The Desired Area According To Movement Of Joystick
-                        transform.position = Vector3.MoveTowards(currPosition, endPosition, speed * relativeSpeed * Time.deltaTime);    // For Actual Movement Of Cube
+                        FindCurrentLocation(pos);
                     }
                 }
             }
