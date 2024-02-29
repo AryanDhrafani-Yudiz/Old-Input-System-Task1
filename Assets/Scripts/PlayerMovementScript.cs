522b37d7f1c281aa7a401f050f87d9fd8766c4d6
AryanDhrafani-Yudiz <aryan.dhrafani@yudiz.com>
2024-02-29 10:05:33 +0530

    changes to not lose

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class PlayerMovementScript : MonoBehaviour
 {
@@ -23,36 +24,70 @@ public class PlayerMovementScript : MonoBehaviour
         height = (float)Screen.height / 2.0f;
         transform.position = new Vector3(0, 0.5f, 0);
         currPosition = transform.position;
-        //Debug.Log(Screen.currentResolution.refreshRateRatio);
+        //Debug.Log(Screen.currentResolution.refreshRate);
     }
     void Update()
     {
-        //Debug.Log(floattoint(Screen.currentResolution.refreshRateRatio));
         if (UImanagerscript.gameplayscreen)     // Move Only If GamePlayScreen Bool Is True
         {
-            if (Input.touchCount == 1)
+            if(Input.mousePresent)
             {
                 if (Input.GetMouseButtonDown(0))    // When First Clicked At A Point On Screen
                 {
-                    Vector2 pos = Input.mousePosition;
-                    pos.x = (pos.x - width) / width;
-                    pos.y = (pos.y - height) / height;
-                    position1 = new Vector3(pos.x, 0.0f, pos.y);
+                        Vector2 pos = Input.mousePosition;
+                        FindStartingLocation(pos);
                 }
                 else if (Input.GetMouseButton(0))   // When The Joystick Is Being Moved
                 {
-                    Vector2 pos = Input.mousePosition;
-                    pos.x = (pos.x - width) / width;
-                    pos.y = (pos.y - height) / height;
-                    position2 = new Vector3(pos.x, 0.0f, pos.y);
-                    diff = (position2 - position1);                           // Difference In Vector3
-                    relativeSpeed = Mathf.Clamp(diff.magnitude, 0.3f, 0.7f);  // Clamps The Magnitude Of Difference (Float Value Of Difference) Between Required Min,Max Values
-                    currPosition = transform.position;
-                    endPosition = currPosition + diff;
-                    transform.LookAt(endPosition);                            // Cube Looks Towards The Desired Area According To Movement Of Joystick
-                    transform.position = Vector3.MoveTowards(currPosition, endPosition, speed * relativeSpeed * Time.deltaTime);    // For Actual Movement Of Cube
+                        Vector2 pos = Input.mousePosition;
+                        FindCurrentLocation(pos);
+                }
+            }
+            else if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                if (!EventSystem.current.IsPointerOverGameObject(Input.GetTouch(0).fingerId))
+                {
+                    if (touch.phase == TouchPhase.Began)    // When First Clicked At A Point On Screen
+                    {
+                        Vector2 pos = touch.position;
+                        pos.x = (pos.x - width) / width;
+                        pos.y = (pos.y - height) / height;
+                        position1 = new Vector3(pos.x, 0.0f, pos.y);
+                    }
+                    else if (touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)   // When The Joystick Is Being Moved
+                    {
+                        Vector2 pos = touch.position;
+                        pos.x = (pos.x - width) / width;
+                        pos.y = (pos.y - height) / height;
+                        position2 = new Vector3(pos.x, 0.0f, pos.y);
+                        diff = (position2 - position1);                           // Difference In Vector3
+                        relativeSpeed = Mathf.Clamp(diff.magnitude, 0.3f, 0.7f);  // Clamps The Magnitude Of Difference (Float Value Of Difference) Between Required Min,Max Values
+                        currPosition = transform.position;
+                        endPosition = currPosition + diff;
+                        transform.LookAt(endPosition);                            // Cube Looks Towards The Desired Area According To Movement Of Joystick
+                        transform.position = Vector3.MoveTowards(currPosition, endPosition, speed * relativeSpeed * Time.deltaTime);    // For Actual Movement Of Cube
+                    }
                 }
             }
         }
+    }
+    void FindStartingLocation(Vector2 pos)
+    {
+        pos.x = (pos.x - width) / width;
+        pos.y = (pos.y - height) / height;
+        position1 = new Vector3(pos.x, 0.0f, pos.y);
+    }
+    void FindCurrentLocation(Vector2 pos)
+    {
+        pos.x = (pos.x - width) / width;
+        pos.y = (pos.y - height) / height;
+        position2 = new Vector3(pos.x, 0.0f, pos.y);
+        diff = (position2 - position1);                           // Difference In Vector3
+        relativeSpeed = Mathf.Clamp(diff.magnitude, 0.3f, 0.7f);  // Clamps The Magnitude Of Difference (Float Value Of Difference) Between Required Min,Max Values
+        currPosition = transform.position;
+        endPosition = currPosition + diff;
+        transform.LookAt(endPosition);                            // Cube Looks Towards The Desired Area According To Movement Of Joystick
+        transform.position = Vector3.MoveTowards(currPosition, endPosition, speed * relativeSpeed * Time.deltaTime);    // For Actual Movement Of Cube
     }
 }
